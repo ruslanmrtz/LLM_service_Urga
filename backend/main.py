ee641f1399ae3908f0648f47c88d1a2cfc0707c3
319 <319@first.local>
2024-10-16 14:08:16 +0500

    added new endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from typing import List
+from sqlalchemy.orm import Session
 
-# from backend.config import SessionLocal
-# from schemas import Dog, DogType, Timestamp
-# from backend.models import ModelsDB
+from backend.config import SessionLocal
+from schemas import Model
+from backend.models import ModelsDB
 
 
 app = FastAPI()
 
 
-# def get_session():
-#     with SessionLocal() as session:
-#         return session
+def get_session():
+    with SessionLocal() as session:
+        return session
 
 
 @app.get('/')
@@ -20,4 +22,40 @@ def root():
     """
     return "root"
 
+
+@app.get('/prompts', response_model=List[Model])
+def get_prompts(db: Session = Depends(get_session)):
+
+    """
+    Получить список prompts
+    """
+
+    result = db.query(ModelsDB).all()
+    return result
+    # return db.query(DogDB).filter(DogDB.kind == kind).all()
+
+
+@app.get('/prompt/{id}', response_model=Model)
+def get_prompt_id(id: int, db: Session = Depends(get_session)):
+    """
+    Получение prompt по id
+    """
+
+    result = db.query(ModelsDB).filter(ModelsDB.id == id).all()
+    return result
+
+
+@app.post('/prompt/{id}', response_model=Model)
+def post_update_prompt(id: int, score: int, comment: str, db: Session = Depends(get_session)):
+    """
+    Обновление оценки и комментария для конкретной комбинации Prompt + LLM
+    """
+
+    model = db.query(ModelsDB).filter(ModelsDB.id == id).first()
+
+    if model is None:
+        raise HTTPException(status_code=404, detail='Dog not found')
+
+    model.score = score
+    model.comment = comment
 
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel
-from enum import Enum
 
 
+class Model(BaseModel):
+
+    id: int
+    project_name: str
+    func_name: str
+    prompt: str
+    llm_name: str
+    score: int
+    comment: str
